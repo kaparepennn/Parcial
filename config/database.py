819ab12c1220cd40bb611d8d257fd413d6dda150
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2025-09-09 20:31:14 +0000

    cambios y comentarios

--- config/database.py
+++ config/database.py
@@ -13,10 +13,8 @@ load_dotenv()
 MYSQL_URI = os.getenv('MYSQL_URI')
 SQLITE_URI = 'sqlite:///libros_local.db'
 
+#Intenta crear una conexión MySQL, si falla, usa una SQLite local.
 def get_engine():
-    """
-    Intenta crear una conexión con MySQL. Si falla, usa SQLite local.
-    """
     if MYSQL_URI:
         try:
             engine = create_engine(MYSQL_URI, echo=True)
@@ -35,8 +33,6 @@ engine = get_engine()
 Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
 
+#Retorna una nueva sesión de base de datos para ser utilizada en los servidores o controladores.
 def get_db_session():
-    """
-    Retorna una nueva sesión de base de datos para ser utilizada en los servicios o controladores.
-    """
     return Session()
