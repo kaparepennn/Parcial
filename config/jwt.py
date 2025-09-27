af18cc4f952f07e75b79836bbc7fe9bf16932119
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2025-09-27 00:23:39 +0000

    guardar jwt

---
+++ config/jwt.py
@@ -0,0 +1,6 @@
+import os
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_TOKEN_LOCATION = ["headers"] 
+JWT_ACCESS_TOKEN_EXPIRES = 3600 #Tiempo definido para el límite de autenticación en la aplicación en segundos, en este caso funciona si lo estas usando o si no. 3600=1hr tiempo estánda
+JWT_HEADER_NAME = "Authorization"
+JWT_HEADER_TYPE = "Bearer"
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from controllers.libros_controllers import libros_bp
 from controllers.users_controllers import user_bp
+from config.jwt import * #Importar jwt de la carpeta config
 
 app = Flask(__name__)
 
+#Agregar el jwt de la carpeta config
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 app.config['JWT_TOKEN_LOCATION'] = JWT_TOKEN_LOCATION
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES #Configurar el tiempo en el que se debe volver a autenticar a la aplicación
-app.config['JWT_HEADER_NAME'] = JWT_HEADER_NAME
+app.config['JWT_HEADER_NAME'] = JWT_HEADER_NAME 
 app.config['JWT_HEADER_TYPE'] = JWT_HEADER_TYPE
 
 # Registrar el blueprint de bandas. Realizar un registro del controlador dentro del main para poder crear un nuevo registro
