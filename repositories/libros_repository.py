31672f27208e3df2a13a7f277ab946c1b62188c5
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2025-10-13 22:19:37 +0000

    repositories

--- repositories/libros_repository.py
+++ repositories/libros_repository.py
@@ -1,40 +1,39 @@
-from models.libros_model import libro, autores
-from sqlalchemy.orm import session
+from models.libros_model import libro
+from sqlalchemy.orm import Session
 
-#Repositorio para la gestión de libros en la base de datos
+class LibrosRepository:
+    def __init__(self, db_session: Session):
+        self.db = db_session
 
-def __init__(self, db_session: session):
-    self.db = db_session
+    # Recuperar todos los libros almacenados en la base de datos
+    def get_all_libros(self):
+        return self.db.query(libro).all()
 
-#Recuperar todos los libros almacenados de la base de datos
-def get_all_libros(self):
-    return self.db.query(libro).all()
+    # Busca y retorna un libro por ID
+    def get_libro_by_id(self, libro_id: int):
+        return self.db.query(libro).filter(libro.id == libro_id).first()
 
-#Busca y retorna un libro por ID
-def get_libro_by_id(self, libro_id: int):
-    return self.db.query(libro).filter(libro.id == libro_id).first()
-
-#Crea y almacena un nuevo libro en la base de datos
-def create_libro(self, name: str):
-    nuevo_libro = libro(name=name)
-    self.db.add(nuevo_libro)
-    self.db.commit()
-    self.db.refresh(nuevo_libro)
-    return nuevo_libro
-
-#Actualizar la información de un libro  existente en la base de datos.
-def actualizar_libro(selft, libro_id: id, name:str = None):
-    libro = selft.get_libro_by_id(libro_id)
-    if libro and name:
-        libro.name = name
-        selft.db.commit()
-        selft.db.refresh(libro)
-    return libro
-
-#Eliminar un libro de la base de datos, según su ID
-def eliminar_libro(self, libro_id: int):
-    libro = self.get_libro_by_id(libro_id)
-    if libro:
-        self.db.delete(libro)
+    # Crea y almacena un nuevo libro en la base de datos
+    def create_libro(self, name: str):
+        nuevo_libro = libro(name=name)
+        self.db.add(nuevo_libro)
         self.db.commit()
-    return libro
+        self.db.refresh(nuevo_libro)
+        return nuevo_libro
+
+    # Actualizar la información de un libro existente
+    def actualizar_libro(self, libro_id: int, name: str = None):
+        libro_obj = self.get_libro_by_id(libro_id)
+        if libro_obj and name:
+            libro_obj.name = name
+            self.db.commit()
+            self.db.refresh(libro_obj)
+        return libro_obj
+
+    # Eliminar un libro por ID
+    def eliminar_libro(self, libro_id: int):
+        libro_obj = self.get_libro_by_id(libro_id)
+        if libro_obj:
+            self.db.delete(libro_obj)
+            self.db.commit()
+        return libro_obj
