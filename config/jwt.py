a1467920578b5ff2f52f150aa2aa997bd6bb58d6
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2025-10-15 21:16:33 +0000

    cambios

--- config/jwt.py
+++ config/jwt.py
@@ -1,7 +1,7 @@
 #Configuración de JWT para autenticación y autorización
 import os
-JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY")
+JWT_SECRET_KEY = os.getenv('JWT_SERCRET_KEY')
 JWT_TOKEN_LOCATION = ["headers"]
 JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hora
-JWT_HEADER_NAME = "Authorization
+JWT_HEADER_NAME = "Authorization"
 JWT_HEADER_TYPE = "Bearer"
