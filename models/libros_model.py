f2474a1a4876e52d850fa8716ad5f4d46a9f6f41
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2025-10-15 21:31:36 +0000

    model

--- models/libros_model.py
+++ models/libros_model.py
@@ -1,18 +1,19 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship
+from models.db import Base
 
-
-Base = declarative_base()
-
-class libro(Base):
-    __tablename__ = "libros"
+libro(Base):
+    __tablename__ = 'libros'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False)
-    autores = relationship("autor", back_populates="libro")
+    autores = relationship('autor', back_populates='libro', cascade='all, delete-orphan')
 
-class autores(Base):
-    __tablename__ = "autores"
+class autor(Base):
+    __tablename__ = 'autores'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    libro_id = Column(Integer, ForeignKey("libros_id"))
-    libro = relationship("libro", back_populates="autores")
+    title = Column(String(255), nullable=False)
+    libro_id = Column(Integer, ForeignKey('libros.id'))
+    libro = relationship('libro', back_populates='autores')
